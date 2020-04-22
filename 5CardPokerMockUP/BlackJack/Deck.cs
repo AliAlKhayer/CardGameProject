18cc07fcae3d9797567182df9983826d0ac042d6
Ali Al Khayer <Ali.Al.Khayer-1@ou.edu>
2020-04-22 11:33:27 -0500

    Steven Deck

--- 5CardPokerMockUP/BlackJack/Deck.cs
+++ 5CardPokerMockUP/BlackJack/Deck.cs
@@ -8,5 +8,24 @@ namespace BlackJack
 {
     class Deck
     {
+
+        private Card[] deck { get; set; }
+        private int currentCard { get; set; }
+        private const int numOfCards = 52;
+        private Random randomNumber { get; set; }
+
+        public Deck()
+        {
+            string[] faces = { "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace" };
+            string[] suits = { "hearts", "diamonds", "spades", "clubs" };
+            deck = new Card[numOfCards];
+            currentCard = 0;
+            randomNumber = new Random();
+
+            for (int i = 0; i < deck.Length; i++)
+            {
+                deck[i] = new Card(faces[i % 11], suits[i / 13]);
+            }
+        }
     }
 }
